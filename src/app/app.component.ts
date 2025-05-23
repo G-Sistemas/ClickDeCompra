b40c55d415874cc3f370aa170e4a2492e0730991
Germann4 <germandiorio8@gmail.com>
2025-05-22 22:04:25 -0300

    feat(login): crear componente para el inicio de sesión

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,18 +4,20 @@ import { ApiService } from './services/api.service';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { inject } from '@angular/core';
 import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
 import { UserRegisterComponentComponent } from './user-register-component/user-register-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterModule, UserRegisterComponentComponent],
+  imports: [CommonModule, RouterModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-   
+  
 }
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { UserRegisterComponentComponent } from './user-register-component/user-register-component.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'register', component: UserRegisterComponentComponent },
+    { path: 'login', component: LoginComponent }
+];
